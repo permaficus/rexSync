de9fd7b1a4682cd8aeac921997335ae70885ea20
permaficus <abukhalif2019@gmail.com>
2024-11-15 21:00:16 +0700

    chore(): refactor redis libs

--- src/libs/redis.ts
+++ src/libs/redis.ts
@@ -1,26 +1,35 @@
 import { createClient, RedisClientType } from "redis";
+import { RedisVaultError } from "./errHandler"
 
 class redisSubscriber {
-    private redis_url: string
-    private redis_channel: `__keyevent@0__:expired`;
+    private redisUrl: string
+    private redisChannel: `__keyevent@0__:expired`;
     private subscriber: RedisClientType | null = null;
 
-    constructor(redis_url: string) {
-        this.redis_url = redis_url;
+    constructor(redisUrl: string) {
+        if (!redisUrl) {
+            this.handleError(`Error: Redis connection URL not specified. Please provide a valid Redis URL to establish the connection.`)
+        }
+        this.redisUrl = redisUrl;
         this.init();
+    }
+
+    private handleError(error: Error | string): void {
+        const message = error instanceof Error ? error.message : error;
+        throw new RedisVaultError(message);
     }
 
     private init (): void {
         if (!this.subscriber) {
             this.subscriber = createClient({
-                url: this.redis_url
+                url: this.redisUrl
             });
             this.subscriber.on('error', (err) => {
-                console.error(`[REDIS] Connection Error: ${err.message || err.code}`);
+                this.handleError(`[REDIS] Connection Error: ${err.message || err.code}`);
             });
     
             this.subscriber.connect().catch((err) => {
-                console.error(`[REDIS] Error during initial connection: ${err.message || err.code}`);
+                this.handleError(`[REDIS] Error during initial connection: ${err.message || err.code}`);
             });
         }
     }
@@ -30,7 +39,7 @@ class redisSubscriber {
     }
  
     channel (): string {
-        return this.redis_channel;
+        return this.redisChannel;
     }
 }
 
