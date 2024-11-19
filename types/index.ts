4fdb6e5a11e42ba9670ca84405a0ae97fb463276
permaficus <abukhalif2019@gmail.com>
2024-11-19 21:35:56 +0700

    chore(DT): refactor type defs

--- types/index.ts
+++ types/index.ts
@@ -1,77 +1,70 @@
 export interface BrokerExchangeInterface {
-    channel: any
-    name: string | undefined | null
-    type: 'direct' | 'fanout' | 'headers' | 'topic'
-    durable: boolean
-    autoDelete?: boolean
-    internal?: boolean
+  channel: any
+  name: string | undefined | null
+  type: 'direct' | 'fanout' | 'headers' | 'topic'
+  durable: boolean
+  autoDelete?: boolean
+  internal?: boolean
 }
 
 export interface QueueTypeInterface {
-    name: string | undefined | null,
-    channel: any,
-    options?: {
-        durable: boolean,
-        arguments?: {
-            'x-queue-type'?: 'classic' | 'quorum' | 'stream',
-            'x-dead-letter-exchange'?: string | string[] | null,
-            'x-dead-letter-routing-key'?: string | string[] | null
-        }
+  name: string | undefined | null
+  channel: any
+  options?: {
+    durable: boolean
+    arguments?: {
+      'x-queue-type'?: 'classic' | 'quorum' | 'stream'
+      'x-dead-letter-exchange'?: string | string[] | null
+      'x-dead-letter-routing-key'?: string | string[] | null
     }
+  }
 }
 export type EventPayload = {
-    key: string
-    expiredAt: string
+  key: string
+  expiredAt: string
 }
-export type AuthSchemes = 
-    | BasicAuth
-    | ApiKey
-    | BearerToken
-    | NoAuth
+export type AuthSchemes = BasicAuth | ApiKey | BearerToken | NoAuth
 
 type BasicAuth = {
-    type: 'basic'
-    username: string
-    password: string
+  type: 'basic'
+  username: string
+  password: string
 }
 type ApiKey = {
-    type: 'apikey'
-    name: string
-    value: string
+  type: 'apikey'
+  name: string
+  value: string
 }
 type BearerToken = {
-    type: 'bearerToken'
-    token: string
+  type: 'bearerToken'
+  token: string
 }
 type NoAuth = {
-    type: 'no-auth'
+  type: 'no-auth'
 }
 type WebhookMethod = {
-    method: 'webhook';
-    url: string;
-    auth?: AuthSchemes;
-};
+  method: 'webhook'
+  url: string
+  auth?: AuthSchemes
+}
 
 type RabbitMQMethod = {
-    method: 'rabbitmq';
-    url: string;
-    exchange: string
-    queue: string
-    routing: string
-};
+  method: 'rabbitmq'
+  url: string
+  exchange: string
+  queue: string
+  routing: string
+}
 
 type FunctionMethod = {
-    method: 'function';
-    onExpiration: (key: string) => Promise<any>;
-};
+  method: 'function'
+  onExpiration: (key: string) => Promise<any>
+}
 
-type TransportMethod = 
-    | WebhookMethod
-    | RabbitMQMethod
-    | FunctionMethod;
+type TransportMethod = WebhookMethod | RabbitMQMethod | FunctionMethod
 
-export type RexSyncInitConfig = {
-    redisUrl: string
-    transport: TransportMethod
-    logExpireKey?: boolean
+export type RexSyncInitArguments = {
+  redisUrl: string
+  transport: TransportMethod
+  logExpireKey?: boolean
 }
